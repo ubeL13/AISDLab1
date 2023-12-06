88711946f61f50f8e37219cd7516c945f7fc44ec
UbeL13 <u.b.2004@yandex.ru>
2023-12-06 08:28:11 +0400

    removed the extra withdrawal in the main

--- App/main.cc
+++ App/main.cc
@@ -9,7 +9,6 @@ using namespace std;
 
 int main()
 {
-	//--------------------------------------------------------------
 	int N;
 	float radius;
 	do
@@ -21,21 +20,6 @@ int main()
 	cin >> radius;
 
 	BrokenLine<float> poly = polygon(N, radius);
-	cout << poly;
-	//--------------------------------------------------------------
-
-	//--------------------------------------------------------------
-	BrokenLine<int> letter = BrokenLine<int>(7);
-	letter[0] = Point<int>(0, 1);
-	letter[1] = Point<int>(0, 0);
-	letter[2] = Point<int>(1, 0);
-	letter[3] = Point<int>(1, 1);
-	letter[4] = Point<int>(1, 0);
-	letter[5] = Point<int>(2, 0);
-	letter[6] = Point<int>(2, 1);
-	cout << letter;
-
-	//--------------------------------------------------------------
 	return 0;
 
 }
